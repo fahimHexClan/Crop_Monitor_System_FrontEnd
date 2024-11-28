e28a8eab3694b13c80dfad685e29687ae3169769
fahim <fahim1998108@gmail.com>
2024-11-28 22:46:00 +0530

    commit field

--- Js/fieldScript.js
+++ Js/fieldScript.js
@@ -1,12 +1,40 @@
-$(document).ready(function () {
-    // Sidebar Toggle
-    $('#toggleSidebar').click(function () {
-        $('#sidebar').toggleClass('collapsed');
-        $('#toggleSidebar i').toggleClass('fa-bars fa-times');
-        $('#mainContent').toggleClass('collapsed');
-    });
-    // Manually Trigger the Modal
-    $('#openModalButton').click(function () {
-        $('#addFieldModal').modal('show');
-    });
+// Sidebar toggle
+document.getElementById('toggleSidebar').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('collapsed');
+});
+
+// Handle form submission for adding field
+document.getElementById('addFieldForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let fieldName = document.getElementById('fieldName').value;
+    let fieldLocation = document.getElementById('fieldLocation').value;
+    let extentSize = document.getElementById('extentSize').value;
+    let cropId = document.getElementById('cropId').value;
+    let staffId = document.getElementById('staffId').value;
+
+    // Add the new field to the table
+    let tableBody = document.getElementById('fieldTableBody');
+    let newRow = tableBody.insertRow();
+
+    newRow.innerHTML = `
+            <td>F002</td>
+            <td>${fieldName}</td>
+            <td>${fieldLocation}</td>
+            <td>${extentSize}</td>
+            <td>${cropId}</td>
+            <td>${staffId}</td>
+            <td><img src="assets/field1.jpg" alt="Field Image 1" class="img-fluid" width="100"></td>
+            <td><img src="assets/field2.jpg" alt="Field Image 2" class="img-fluid" width="100"></td>
+            <td>
+                <button class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#updateFieldModal"><i class="fas fa-edit"></i></button>
+                <button class="btn btn-outline-danger btn-sm"><i class="fas fa-trash"></i></button>
+            </td>
+        `;
+
+    // Clear the form
+    document.getElementById('addFieldForm').reset();
+    // Close the modal
+    var addModal = new bootstrap.Modal(document.getElementById('addFieldModal'));
+    addModal.hide();
 });
