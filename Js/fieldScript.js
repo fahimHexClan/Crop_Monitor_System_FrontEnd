bb6215ae2c562243279dfe7be502b55baf64d4f7
fahim <fahim1998108@gmail.com>
2024-11-29 05:46:27 +0530

    commit field

--- Js/fieldScript.js
+++ Js/fieldScript.js
@@ -10,8 +10,7 @@ document.getElementById('addFieldForm').addEventListener('submit', function(e) {
     let fieldName = document.getElementById('fieldName').value;
     let fieldLocation = document.getElementById('fieldLocation').value;
     let extentSize = document.getElementById('extentSize').value;
-    let cropId = document.getElementById('cropId').value;
-    let staffId = document.getElementById('staffId').value;
+    let LogId = document.getElementById('LogId').value;
 
     // Add the new field to the table
     let tableBody = document.getElementById('fieldTableBody');
@@ -22,10 +21,9 @@ document.getElementById('addFieldForm').addEventListener('submit', function(e) {
             <td>${fieldName}</td>
             <td>${fieldLocation}</td>
             <td>${extentSize}</td>
-            <td>${cropId}</td>
-            <td>${staffId}</td>
             <td><img src="assets/field1.jpg" alt="Field Image 1" class="img-fluid" width="100"></td>
             <td><img src="assets/field2.jpg" alt="Field Image 2" class="img-fluid" width="100"></td>
+            <td>${LogId}</td>
             <td>
                 <button class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#updateFieldModal"><i class="fas fa-edit"></i></button>
                 <button class="btn btn-outline-danger btn-sm"><i class="fas fa-trash"></i></button>
